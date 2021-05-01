5aec93bc78a98aea94ad47a1f310fae7645ae5df
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-02 00:46:37 +1000

    create react app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+//const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,10 +13,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+//app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Google-Books");
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
