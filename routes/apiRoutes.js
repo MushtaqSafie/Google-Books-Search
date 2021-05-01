4cceb3d6e3faec8b54417f5ca79d7448fc3c1d6a
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-02 00:50:59 +1000

    config API routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/books", (req, res) => {
+  // get data from database and response with JSON
+
+});
+
+module.exports = router;
