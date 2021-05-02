9a36318378281a8a6febf70e1168d5cb84e60bee
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-02 14:48:35 +1000

    update navbar components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { useStoreContext } from "../../utils/GlobalState";
+import logo from "../../logo.png";
 
 function Nav() {
   const [store] = useStoreContext();
+  const location = useLocation().pathname;
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
       <a className="navbar-brand" href="/search">
-        Search
+        <img src={logo}  height="50" className="d-inline-block align-top" alt="logo" />
       </a>
-      <a className="navbar-brand" href="/saved">
-        Saved
-      </a>
+
+      <ul className="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style={{maxHeight: "100px"}}>
+        <li className={"nav-item "+ ("/search"===location&&"active")}>
+          <a className="nav-link" href="/search" >Search</a>
+        </li>
+        <li className={"nav-item "+ ("/saved"===location&&"active")}>
+          <a className="nav-link" href="/saved">Saved</a>
+        </li>
+      </ul>
+
        {/* eslint-disable-next-line */}
       {store.loading ? <a className="navbar-brand ml-auto">Loading...</a> : <></>}
     </nav>
