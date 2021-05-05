05ec7526c74d96d7afe87100e3d779574949477c
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-05 17:08:26 +1000

    input field validation

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Col, Row } from "../../components/Grid";
 import { useStoreContext } from "../../utils/GlobalState";
 import { BOOKS_RESULT, RESET_RESULT } from '../../utils/actions';
@@ -6,23 +6,30 @@ import API from "../../utils/API"
 
 function SearchForm() {
   const [state, dispatch] = useStoreContext();
-
+  
   const keywordRef = useRef();
+  const [validKeyword, setvalidKeyword] = useState(true);
 
   const handleSearch = (e) => {
     e.preventDefault();
-    dispatch({ type: RESET_RESULT });
-    API.getSearch(keywordRef.current.value)
-      .then(results => {
-        console.log(results);
-        dispatch({
-          type: BOOKS_RESULT,
-          books: results.data
-        })
-      })
-      .catch(err => console.log(err));
+    // if the input field is empty alert to type a keyword
+    keywordRef.current.value ? setvalidKeyword(!0) : setvalidKeyword(!1);
 
-    keywordRef.current.value = "";
+    if (keywordRef.current.value) {
+      dispatch({ type: RESET_RESULT });
+      API.getSearch(keywordRef.current.value)
+        .then(results => {
+          console.log(results);
+          dispatch({
+            type: BOOKS_RESULT,
+            books: results.data
+          })
+        })
+        .catch(err => console.log(err));
+  
+      keywordRef.current.value = "";
+    }
+
   }
 
   return (
@@ -33,6 +40,11 @@ function SearchForm() {
           <div className="form-group">
             <input ref={keywordRef} type="text" className="form-control  col-12"  aria-describedby="searchKeyword" />
           </div>
+          {!validKeyword &&
+          <div className="alert alert-danger" role="alert">
+            Enter a valid keyword to search for Book!
+          </div>
+          }
 
           <button type="submit" className="btn btn-primary ">Find Book</button>
         </form>
